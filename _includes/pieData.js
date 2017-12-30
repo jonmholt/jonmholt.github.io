e502418e74543d6057f5ccae08ecbb6589030633
Jon Holt <jon@twentyfivetwenty.ca>
2017-12-30 12:41:13 -0700

    two

--- _includes/pieData.js
+++ _includes/pieData.js
@@ -42,3 +42,69 @@ var pieData = [
         label: "Premium Rentals"
     }
 ];
+var postData = {
+    labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+    datasets: [
+        {
+            label: "Blog posts",
+            fillColor: "rgba(220,220,220,0.5)",
+            strokeColor: "rgba(220,220,220,0.8)",
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+            data: [1, 0, 0, 0, 13, 20, 17, 16, 10, 0, 0, 0]
+        }
+    ]
+};
+var barChartData = {
+    labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+    datasets: [
+        {
+            label: "Servus",
+            fillColor: "rgba(220,220,220,0.5)",
+            strokeColor: "rgba(220,220,220,0.8)",
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+            data: [15, 7, 16, 48, 60, 7, 0, 4, 32, 15, 8, 15]
+        },
+        {
+            label: "CUCA",
+            fillColor: "rgba(151,187,205,0.5)",
+            strokeColor: "rgba(151,187,205,0.8)",
+            highlightFill: "rgba(151,187,205,0.75)",
+            highlightStroke: "rgba(151,187,205,1)",
+            data: [0, 18, 0, 24, 18, 96, 0, 0, 28, 19, 72, 24]
+        },
+        {
+            label: "AUMA",
+            fillColor: "rgba(240,73,73,0.5)",
+            strokeColor: "rgba(240,73,73,0.8)",
+            highlightFill: "rgba(240,73,73,0.75)",
+            highlightStroke: "rgba(240,73,73,1)",
+            data: [90, 79.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        },
+        {
+            label: "NRCB",
+            fillColor: "rgba(205,73,187,0.5)",
+            strokeColor: "rgba(205,73,187,0.8)",
+            highlightFill: "rgba(205,73,187,0.75)",
+            highlightStroke: "rgba(205,73,187,1)",
+            data: [54, 49, 60, 63, 60.5, 40, 62, 50, 50, 59.5, 40, 52.5]
+        },
+        {
+            label: "Premium Rentals",
+            fillColor: "rgba(187,220,151,0.5)",
+            strokeColor: "rgba(187,220,151,0.8)",
+            highlightFill: "rgba(187,220,151,0.75)",
+            highlightStroke: "rgba(187,220,151,1)",
+            data: [0, 0, 27, 10.5, 2.5, 5.5, 4, 4, 4, 0, 0, 1.5]
+        },
+        {
+            label: "DHR",
+            fillColor: "rgba(40,40,40,0.5)",
+            strokeColor: "rgba(40,40,40,0.8)",
+            highlightFill: "rgba(40,40,40,0.75)",
+            highlightStroke: "rgba(40,40,40,1)",
+            data: [15, 15, 15, 15, 15, 15, 45, 0, 45, 45, 45, 45]
+        }
+    ]
+};
