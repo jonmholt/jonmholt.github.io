91536653cf61ea9bf010c84849eedd6db8b50b99
Jon Holt <jon@twentyfivetwenty.ca>
2017-12-30 12:39:50 -0700

    one

--- _includes/pieData.js
+++ _includes/pieData.js
@@ -1,1 +1,44 @@
-var test = "test";
+var pieData = [
+    {
+        value: 62800.00,
+        color: "#949FB1",
+        highlight: "#A8B3C5",
+        label: "NRCB"
+    },
+    {
+        value: 18651.43,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "CUCA"
+    },
+    {
+        value: 20340.00,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "AUMA"
+    },
+    {
+        value: 4944.00,
+        color: "#5CB4FD",
+        highlight: "#FFC870",
+        label: "DHR"
+    },
+    {
+        value: 41128.40,
+        color: "#F7464A",
+        highlight: "#FF5A5E",
+        label: "Servus"
+    },
+    {
+        value: 11846.55,
+        color: "#4D5360",
+        highlight: "#616774",
+        label: "EPSB"
+    },
+    {
+        value: 5700.00,
+        color: "#efefef",
+        highlight: "#616774",
+        label: "Premium Rentals"
+    }
+];
